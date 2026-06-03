d2c8203a60035ddb7ff0a700a519102a056b6eb7
Eugene Skachkov <zhenyfaceit@mail.ru>
2026-06-03 12:42:29 +0300

    add title and descr

--- resources/views/layouts/fix-masters.blade.php
+++ resources/views/layouts/fix-masters.blade.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'FIX-MASTERS')</title>
+  @hasSection('description')
+  <meta name="description" content="@yield('description')">
+  @endif
   <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
